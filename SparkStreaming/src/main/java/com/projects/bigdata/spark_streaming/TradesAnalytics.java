8b7b3c2021e6c71d65109f115c6dfdb7d544e017
nicolanardino <nicola.nardino@nicolanardino.net>
2019-01-15 19:59:33 +0100

    Removed boilerplate class implementing Function3 with method reference.

--- SparkStreaming/src/main/java/com/projects/bigdata/spark_streaming/TradesAnalytics.java
+++ SparkStreaming/src/main/java/com/projects/bigdata/spark_streaming/TradesAnalytics.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.spark.api.java.Optional;
-import org.apache.spark.api.java.function.Function3;
 import org.apache.spark.streaming.State;
 import org.apache.spark.streaming.StateSpec;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -39,28 +38,22 @@ public final class TradesAnalytics extends AbstractStreaming {
     	final JavaDStream<Trade> unionStream = streamingContext.socketTextStream(dataStreamHost1, dataStreamPort1).map(TradesAnalytics::convertJsonToTrade)
     			.union(streamingContext.socketTextStream(dataStreamHost2, dataStreamPort2).map(TradesAnalytics::convertJsonToTrade));
     	final JavaPairDStream<String, Trade> symbolToTrade = unionStream.mapToPair(t -> new Tuple2<String, Trade>(t.getSymbol(), t));
-    	final JavaMapWithStateDStream<String, Trade, List<Trade>, Tuple2<String, Double>> mapWithState = symbolToTrade.mapWithState(StateSpec.function(new StatefulAggregation()));
+    	final JavaMapWithStateDStream<String, Trade, List<Trade>, Tuple2<String, Double>> mapWithState = symbolToTrade.mapWithState(StateSpec.function(TradesAnalytics::tradesAggregation));
     	mapWithState.print();
     	startStreamingAndAwaitTerminationOrTimeout(processingTimeout);
     }
     
-    private static class StatefulAggregation implements Function3<String, Optional<Trade>, State<List<Trade>>, Tuple2<String, Double>> {
-
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public Tuple2<String, Double> call(final String symbol, Optional<Trade> currentTrade, State<List<Trade>> state) throws Exception {         
-            final List<Trade> tradesState = state.exists() ? state.get() : new ArrayList<>(); 
-            logger.info("Current Trade:\n"+currentTrade.orNull());
-            logger.info("Current Trades:\n"+tradesState.stream().map(t -> t.toString()).collect(Collectors.joining("\n")));
-            if (currentTrade.isPresent()) 
-            	tradesState.add(currentTrade.get());  
-            final Double priceAverage = tradesState.stream().mapToDouble(t -> t.getPrice().doubleValue()).average().orElse(0.0); 
-            logger.info("Current Price Average: "+priceAverage);
-            state.update(tradesState); 
-            return new Tuple2<String, Double>(symbol, priceAverage); 
-		}
-    }
+    private static Tuple2<String, Double> tradesAggregation(final String symbol, Optional<Trade> currentTrade, State<List<Trade>> state) throws Exception {         
+        final List<Trade> tradesState = state.exists() ? state.get() : new ArrayList<>(); 
+        logger.info("Current Trade:\n"+currentTrade.orNull());
+        logger.info("Current Trades:\n"+tradesState.stream().map(t -> t.toString()).collect(Collectors.joining("\n")));
+        if (currentTrade.isPresent()) 
+        	tradesState.add(currentTrade.get());  
+        final Double priceAverage = tradesState.stream().mapToDouble(t -> t.getPrice().doubleValue()).average().orElse(0.0); 
+        logger.info("Current Price Average: "+priceAverage);
+        state.update(tradesState); 
+        return new Tuple2<String, Double>(symbol, priceAverage); 
+	}
     
     private static Trade convertJsonToTrade(final String jsonTrade) throws JsonParseException, JsonMappingException, IOException {
     	return mapper.readValue(jsonTrade, Trade.class);
