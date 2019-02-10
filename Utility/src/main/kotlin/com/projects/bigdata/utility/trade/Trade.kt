da46ee2fc365e18de0d2bea0bcf56dd0f04e3bd5
nicolanardino <nicola.nardino@nicolanardino.net>
2019-02-10 19:42:20 +0100

    Added Trade Comparable implementation.

--- Utility/src/main/kotlin/com/projects/bigdata/utility/trade/Trade.kt
+++ Utility/src/main/kotlin/com/projects/bigdata/utility/trade/Trade.kt
@@ -3,9 +3,11 @@ package com.projects.bigdata.utility.trade
 import java.io.Serializable
 import java.math.BigDecimal
 
-data class Trade(val symbol: String, val direction: Direction, val quantity: Int, val price: BigDecimal, val exchange: Exchange) : Serializable {
+data class Trade(val symbol: String, val direction: Direction, val quantity: Int, val price: BigDecimal, val exchange: Exchange) : Serializable, Comparable<Trade> {
     //Needed for the JSON deserialization.
     private constructor() : this("", Direction.Buy, 0, BigDecimal(0.0), Exchange.EUREX)
+
+    override fun compareTo(other: Trade) = compareValuesBy(this, other, Trade::symbol, Trade::direction, Trade::exchange)
 
     companion object {
         private const val serialVersionUID = 1L
