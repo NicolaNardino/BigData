5b7c22fb494e060e0c3851f6decfb09e505ad1c6
nicolanardino <nicola.nardino@nicolanardino.net>
2019-04-07 21:08:49 +0200

    Minor changes.

--- CassandraMicroservice/src/main/kotlin/com/projects/bigdata/cassandra_microservice/CassandraRestController.kt
+++ CassandraMicroservice/src/main/kotlin/com/projects/bigdata/cassandra_microservice/CassandraRestController.kt
@@ -4,7 +4,6 @@ import com.projects.bigdata.utility.trade.Direction
 import com.projects.bigdata.utility.trade.Exchange
 import com.projects.bigdata.utility.trade.Trade
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -20,16 +19,16 @@ class CassandraRestController {
     lateinit var cassandraTradeRepository: CassandraTradeRepository
 
     @GetMapping("/getAllTrades", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getAllTrades(): ResponseEntity<List<Trade>> = ResponseEntity(cassandraTradeRepository.findAll(), HttpStatus.OK)
+    fun getAllTrades(): ResponseEntity<List<Trade>> = ResponseEntity.ok(cassandraTradeRepository.findAll())
 
     @GetMapping("/getTradesByExchange/{exchange}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getTradesByExchange(@PathVariable exchange: Exchange): ResponseEntity<List<Trade>> = ResponseEntity.ok(cassandraTradeRepository.findByExchange(exchange))
 
     @GetMapping("/getTradesByExchangeAndDirection/{exchange}/{direction}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getTradesByExchangeAndDirection(@PathVariable exchange: Exchange, @PathVariable direction: Direction): ResponseEntity<List<Trade>> =
-            ResponseEntity(cassandraTradeRepository.findByExchangeAndDirection(exchange, direction), HttpStatus.OK)
+            ResponseEntity.ok(cassandraTradeRepository.findByExchangeAndDirection(exchange, direction))
 
     @GetMapping("/getTradesByExchangeAndDirectionAndSymbol/{exchange}/{direction}/{symbol}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getTradesByExchangeAndDirection(@PathVariable exchange: Exchange, @PathVariable direction: Direction, @PathVariable symbol: String): ResponseEntity<List<Trade>> =
-            ResponseEntity(cassandraTradeRepository.findByExchangeAndDirectionAndSymbol(exchange, direction, symbol), HttpStatus.OK)
+            ResponseEntity.ok(cassandraTradeRepository.findByExchangeAndDirectionAndSymbol(exchange, direction, symbol))
 }
