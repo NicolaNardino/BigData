d653c16ef3aacd5fba22030e0f54849e1235802f
nicolanardino <nicola.nardino@nicolanardino.net>
2019-08-25 20:45:15 +0200

    Added delta tables append/ overwrite test.

--- DeltaLake/src/main/kotlin/com/projects/bigdata/delta_lake/DeltaLakeSparkTest.kt
+++ DeltaLake/src/main/kotlin/com/projects/bigdata/delta_lake/DeltaLakeSparkTest.kt
@@ -1,11 +1,13 @@
 package com.projects.bigdata.delta_lake
 
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.SparkSession
 
 fun main () {
-    val outputFolder = "~/Projects/DeltaLake/Tables/spark-range-delta-table"
+    val outputFolder = "DeltaTables/spark-range-delta-table"
     val spark = SparkSession.builder().appName("DeltaLateTest").master("local[*]").orCreate
-    spark.range(0,100).write().format("delta").save(outputFolder);
-    val df = spark.read().format("delta").load(outputFolder)
-    df.show()
+    spark.range(0,100).write().format("delta").mode(SaveMode.Overwrite).save(outputFolder);
+    spark.range(0,1000).write().format("delta").mode(SaveMode.Append).save(outputFolder);
+    val loadedDs = spark.read().format("delta").load(outputFolder)
+    loadedDs.show()
 }
